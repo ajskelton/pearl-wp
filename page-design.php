7b369aae66aee1f3b54977fc66e77d5e6c29e1fa
Anthony Skelton <anthony@redcartstudios.com>
2015-11-15 05:40:38 -0800

    Added ACF Integration for design elements

--- page-design.php
+++ page-design.php
@@ -7,6 +7,19 @@
 
 <?php get_header(); ?>
 
+		<?php
+
+			$image_1 = get_field('image_1');
+			$image_2 = get_field('image_2');
+			$image_3 = get_field('image_3');
+			$image_4 = get_field('image_4');
+			$image_5 = get_field('image_5');
+			$image_6 = get_field('image_6');
+			$image_7 = get_field('image_7');
+			$image_8 = get_field('image_8');
+
+		?>
+
 			<div id="content">
 
 				<div id="inner-content" class="inner-content">
@@ -14,20 +27,36 @@
 					<div class="container entry-content">
 						
 						<div class="design">
-							<img src="http://placehold.it/400x300" alt="">
-							<img src="http://placehold.it/400x600" alt="">
+							<?php if( !empty($image_1) ): ?>
+								<img src="<?= $image_1['url']; ?>" alt="<?= $image_1['alt']; ?>">
+							<?php endif; ?>
+							<?php if( !empty($image_2) ): ?>
+								<img src="<?= $image_2['url']; ?>" alt="<?= $image_2['alt']; ?>">
+							<?php endif; ?>
 						</div>
 						<div class="design">
-							<img src="http://placehold.it/400x600" alt="">
-							<img src="http://placehold.it/400x300" alt="">
+							<?php if( !empty($image_3) ): ?>
+								<img src="<?= $image_3['url']; ?>" alt="<?= $image_3['alt']; ?>">
+							<?php endif; ?>
+							<?php if( !empty($image_4) ): ?>
+								<img src="<?= $image_4['url']; ?>" alt="<?= $image_4['alt']; ?>">
+							<?php endif; ?>
 						</div>
 						<div class="design">
-							<img src="http://placehold.it/400x300" alt="">
-							<img src="http://placehold.it/400x600" alt="">
+							<?php if( !empty($image_5) ): ?>
+								<img src="<?= $image_5['url']; ?>" alt="<?= $image_5['alt']; ?>">
+							<?php endif; ?>
+							<?php if( !empty($image_6) ): ?>
+								<img src="<?= $image_6['url']; ?>" alt="<?= $image_6['alt']; ?>">
+							<?php endif; ?>
 						</div>
 						<div class="design">
-							<img src="http://placehold.it/400x600" alt="">
-							<img src="http://placehold.it/400x300" alt="">
+							<?php if( !empty($image_7) ): ?>
+								<img src="<?= $image_7['url']; ?>" alt="<?= $image_7['alt']; ?>">
+							<?php endif; ?>
+							<?php if( !empty($image_8) ): ?>
+								<img src="<?= $image_8['url']; ?>" alt="<?= $image_8['alt']; ?>">
+							<?php endif; ?>
 						</div>
 
 					</div> <!-- End container -->
